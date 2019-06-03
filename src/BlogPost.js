ca7276ea972842ed11705bb03c3da32ab3f35780
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-06-03 20:30:41 +0000

    Automatically backed up by Learn

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // are we missing an import?
-import Comment from '/Comment';
+import Comment from '/Comment.js';
 
 export default class BlogPost extends Component {
   render() {
