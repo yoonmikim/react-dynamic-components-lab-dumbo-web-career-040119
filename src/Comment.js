3d7b9bfe0eb11aa23f553bff5d5b9d79305bf543
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-06-03 20:27:43 +0000

    Automatically backed up by Learn

--- src/Comment.js
+++ src/Comment.js
@@ -6,18 +6,6 @@ export default class Comment extends Component {
     
     return (
       <div className="comment">
-        Alright internet trolls<br/>
-        Come at me<br/>
-        I have been preparing<br/>
-        My meme fortress is built<br/>
-        I won't let you tilt me<br/>
-        You only grew accustomed to the salt<br/>
-        I was born in it<br/>
-        {/* (remove the comment ticks and their surrounding brackets)
-        <Comment commentText={comments[0]} />
-        <Comment commentText={comments[0]} />
-        <Comment commentText={comments[0]} />
-        */}
       </div>
     );
   }
